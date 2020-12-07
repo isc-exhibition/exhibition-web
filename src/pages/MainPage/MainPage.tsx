be1a30d6356f038706330d864fc339dcf615946e
JSKeum <JSKeum2512@gmail.com>
2020-12-07 21:37:33 +0900

    🎨 add scss

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import styles from './MainPage.module.scss';
 
 function MainPage() {
-  return <h1>ISC Main Page</h1>;
+  return <h1 className={styles.header}>ISC Main Page</h1>;
 }
 
 export default MainPage;
