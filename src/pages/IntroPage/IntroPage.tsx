93a5f66ada6f32080b2138789eee8371678c5092
JSKeum <JSKeum2512@gmail.com>
2020-12-07 17:48:57 +0900

    ✨ init structure of components

---
+++ src/pages/IntroPage/IntroPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function IntroPage() {
+  return <h1>ISC Intro Page</h1>;
+}
+
+export default IntroPage;
---
+++ src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function MainPage() {
+  return <h1>ISC Main Page</h1>;
+}
+
+export default MainPage;
