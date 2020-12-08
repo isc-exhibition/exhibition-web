7f779911a47cf5a1d31c729e3a38ccf9f679413f
vivapyk <vivapyk@gmail.com>
2020-12-09 03:02:49 +0900

    :sparkles:add IntroPage

--- src/pages/IntroPage/IntroPage.tsx
+++ src/pages/IntroPage/IntroPage.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import IntroImage from './assets/intro_page.png';
+import IntroEnter from './assets/enter.png';
+import styles from './IntroPage.module.scss';
 
 function IntroPage() {
-  return <h1>ISC Intro Page</h1>;
+  return (
+    <div className={styles.IntroContainer}>
+      <img src={IntroImage} alt="IntroImage" className={styles.IntroImage} />
+      <img src={IntroEnter} alt="IntroEnter" className={styles.IntroEnter} />
+    </div>
+  );
 }
-
 export default IntroPage;
