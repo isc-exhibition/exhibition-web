88dfda7d83d972e3cda789115263aaf8ef59e767
vivapyk <vivapyk@gmail.com>
2020-12-12 17:36:31 +0900

    :art: fix IntroPage format

--- src/pages/IntroPage/IntroPage.tsx
+++ src/pages/IntroPage/IntroPage.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import IntroImage from './assets/MobileIntro.png';
 import IntroEnter from './assets/enter.png';
 import styles from './IntroPage.module.scss';
 
 function IntroPage() {
   return (
     <div className={styles.IntroContainer}>
-      <img src={IntroImage} alt="IntroImage" className={styles.IntroImage} />
-      <img src={IntroEnter} alt="IntroEnter" className={styles.IntroEnter} />
+      <div className={styles.IntroBackground}>
+        <div>
+          <img src={IntroEnter} alt="IntroEnter" className={styles.IntroEnter} />
+        </div>
+      </div>
     </div>
   );
 }
