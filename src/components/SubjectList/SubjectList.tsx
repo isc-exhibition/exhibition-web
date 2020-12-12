5b1fc171c71c9be3136cc23b85d188645d036c9f
9ummy <godante27@gmail.com>
2020-12-12 13:50:19 +0900

    :art: add info npc

--- src/components/SubjectList/SubjectList.tsx
+++ src/components/SubjectList/SubjectList.tsx
@@ -4,18 +4,23 @@ import subjectListImage from '../../assets/subjectList.png';
 
 function SubjectList() {
   return (
-    <div className={styles.subjectList} style={{ backgroundImage: `url(${subjectListImage})` }}>
-      <div className={styles.subjectListColumn}>
-        <h1>Subject</h1>
-        <h1>Subject</h1>
-        <h1>Subject</h1>
-        <h1>Subject</h1>
+    <div className={styles.subjectListBorder}>
+      <div>
+        <img className={styles.subjectListImage} src={subjectListImage} alt="" />
       </div>
-      <div className={styles.subjectListColumn}>
-        <h1>Subject</h1>
-        <h1>Subject</h1>
-        <h1>Subject</h1>
-        <h1>Subject</h1>
+      <div className={styles.subjectList}>
+        <div className={styles.subjectListColumn}>
+          <h1>Subject</h1>
+          <h1>Subject</h1>
+          <h1>Subject</h1>
+          <h1>Subject</h1>
+        </div>
+        <div className={styles.subjectListColumn}>
+          <h1>Subject</h1>
+          <h1>Subject</h1>
+          <h1>Subject</h1>
+          <h1>Subject</h1>
+        </div>
       </div>
     </div>
   );
