e90c21b8993e0fb2498d30cf5a98b54d1b23e1fa
vivapyk <vivapyk@gmail.com>
2020-12-12 21:23:28 +0900

    :art: IntroPage layout

--- src/pages/IntroPage/IntroPage.tsx
+++ src/pages/IntroPage/IntroPage.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import IntroEnter from './assets/enter.png';
+import Logo from './assets/logo.png';
 import styles from './IntroPage.module.scss';
 
 function IntroPage() {
   return (
     <div className={styles.IntroContainer}>
-      <div className={styles.IntroBackground}>
-        <div>
-          <img src={IntroEnter} alt="IntroEnter" className={styles.IntroEnter} />
-        </div>
+      <div className={styles.IntroImage}>
+        <img src={Logo} alt="Logo" className={styles.Logo} />
+        <img src={IntroEnter} alt="IntroEnter" className={styles.IntroEnter} />
       </div>
     </div>
   );
