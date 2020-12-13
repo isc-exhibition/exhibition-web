d323e03651a6e58dce3ddef0c29d45e5be13e875
9ummy <godante27@gmail.com>
2020-12-13 17:09:12 +0900

    :art: fix LogoButton

--- src/components/LogoButton/LogoButton.tsx
+++ src/components/LogoButton/LogoButton.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Link,
-} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import styles from './LogoButton.module.scss';
 import logoButton from '../../assets/mobile_logo.png';
 
 function LogoButton() {
-  return <Router><Link to="/main"><img className={styles.logoButton} src={logoButton} alt="" /></Link></Router>;
+  return <Link to="/main"><img className={styles.logoButton} src={logoButton} alt="" /></Link>;
 }
 
 export default LogoButton;
