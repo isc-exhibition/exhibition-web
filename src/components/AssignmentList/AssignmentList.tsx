d8fc8a637c67c1d94d669ddc0dfbae6648f4d35e
JSKeum <JSKeum2512@gmail.com>
2020-12-15 01:51:39 +0900

    :art: fix assignment list style

--- src/components/AssignmentList/AssignmentList.tsx
+++ src/components/AssignmentList/AssignmentList.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styles from './AssignmentList.module.scss';
 import trayBackgroundImage from './subject_tray.png';
 
 function AssignmentList() {
+  const mockupAssignmentList = [
+    { name: '과제1', id: '1' },
+    { name: '과제1', id: '1' },
+    { name: '과제1', id: '1' },
+    { name: '과제1', id: '1' },
+    { name: '과제1', id: '1' },
+    { name: '과제1', id: '1' },
+  ];
+
   return (
-    <div className={styles.assignmnetList} style={{ backgroundImage: `url(${trayBackgroundImage})` }} />
+    <div
+      className={styles.assignmnetList}
+      style={{ backgroundImage: `url(${trayBackgroundImage})` }}
+    >
+      {mockupAssignmentList.map((assignment) => (
+        <Link to={`/subjectdetail/${assignment.id}`}>
+          <div className={styles.assignment}><h1>{assignment.name}</h1></div>
+        </Link>
+      ))}
+    </div>
+
   );
 }
 
