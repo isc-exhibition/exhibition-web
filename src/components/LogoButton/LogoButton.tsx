bc9cae1a391f229f3176cd87503451c04cc018bc
9ummy <godante27@gmail.com>
2020-12-15 05:25:33 +0900

    :art: add desktop responsive view

--- src/components/LogoButton/LogoButton.tsx
+++ src/components/LogoButton/LogoButton.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './LogoButton.module.scss';
-import logoButton from '../../assets/mobile_logo.png';
+import mobileLogoButtonImage from '../../assets/mobile_logo.png';
+import desktopLogoButtonImage from '../../assets/desktop_logo.png';
+import UseMediaQuery from '../../customHooks/UseMediaQuery';
 
 function LogoButton() {
-  return <Link to="/main"><img className={styles.logoButton} src={logoButton} alt="" /></Link>;
+  const isDeviceWidthWideAsDesktop = UseMediaQuery('(min-width: 800px)');
+
+  const logoButtonImageUrl = isDeviceWidthWideAsDesktop
+    ? desktopLogoButtonImage : mobileLogoButtonImage;
+
+  return <Link to="/main"><img className={styles.logoButton} src={logoButtonImageUrl} alt="" /></Link>;
 }
 
 export default LogoButton;
