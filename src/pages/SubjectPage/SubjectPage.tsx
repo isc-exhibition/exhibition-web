768792531bf83f0a8840a0f7d2789e23a0d80c2c
JSKeum <JSKeum2512@gmail.com>
2020-12-15 01:53:00 +0900

    :art: fix subject page tray

--- src/pages/SubjectPage/SubjectPage.tsx
+++ src/pages/SubjectPage/SubjectPage.tsx
@@ -1,6 +1,4 @@
-import React, {
-  useRef, useState, TouchEvent, CSSProperties,
-} from 'react';
+import React, { CSSProperties } from 'react';
 import styles from './SubjectPage.module.scss';
 import subjectBackgroundImage from './mobile_subject_background.png';
 import AssignmentList from '../../components/AssignmentList/AssignmentList';
@@ -24,91 +22,105 @@ function SubjectPage() {
   const isDeviceHighHighAsIphone8Plus = UseMediaQuery('(min-height: 730px)');
 
   const subject = mockupSubject;
-  const trayDefaultTop = '88vh';
+  // const trayDefaultTop = '88vh';
 
-  const [offsetTopByDragup, setOffsetTopByDragup] = useState(0);
-  const [isAssignmentListTrayDragUp, setAssignmentListTrayDragUp] = useState(false);
-  const [assignmentListTrayDragReset, setAssignmentListTrayDragReset] = useState(false);
-  const [trayOffsetJustBefore, setTrayOffsetJustBefore] = useState(0);
-  const assignmentListTray = useRef<HTMLDivElement>(null);
+  // const onTraySwipeStart = ()
 
-  const handleSubjectListWrapperOnToucStart = (event: TouchEvent<HTMLDivElement>) => {
-    setAssignmentListTrayDragReset(false);
-    setTrayOffsetJustBefore(event.changedTouches[0].screenY);
-  };
-  const handleSubjectListWrapperOnTouchMove = (event: TouchEvent<HTMLDivElement>) => {
-    const currentScreenY = event.changedTouches[0].screenY;
-    const isDragginUp = currentScreenY <= trayOffsetJustBefore;
-    // const isDragginDown = currentScreenY >= trayOffsetJustBefore;
+  // const onTraySwipeStart = (event: any) => {
+  //   console.log('Start swiping...', event);
+  // };
 
-    if (!isAssignmentListTrayDragUp) {
-      if (assignmentListTray.current
-      && !isAssignmentListTrayDragUp
-      && isDragginUp
-      && offsetTopByDragup <= 500
-      ) {
-        const offset = offsetTopByDragup + 1.8;
-        setOffsetTopByDragup(offset);
-        setTrayOffsetJustBefore(currentScreenY);
-      }
-      if (assignmentListTray.current
-      && !isAssignmentListTrayDragUp
-      && !isDragginUp
-      && offsetTopByDragup >= 0
-      ) {
-        const offset = offsetTopByDragup - 1.8;
-        setOffsetTopByDragup(offset);
-        setTrayOffsetJustBefore(currentScreenY);
-      }
-    }
-    // else if (assignmentListTray.current
-    //   && isAssignmentListTrayDragUp
-    //   && isDragginDown
-    //   && offsetTopByDragup <= 0
-    // ) {
-    //   console.log(123123);
-    //   const offset = offsetTopByDragup - 1.4;
-    //   console.log(offset);
-    //   setOffsetTopByDragup(offset);
-    //   setTrayOffsetJustBefore(currentScreenY);
-    // }
-  };
+  // const onTraySwipeMove = (position: any, event: any) => {
+  //   console.log(`Moved ${position.x} pixels horizontally`, event);
+  //   console.log(`Moved ${position.y} pixels vertically`, event);
+  // };
 
-  const handleSubjectListWrapperOnTouchEnd = () => {
-    if (offsetTopByDragup > 50) {
-      setOffsetTopByDragup(0);
-      setAssignmentListTrayDragUp(true);
-    } else {
-      setAssignmentListTrayDragReset(true);
-      setOffsetTopByDragup(0);
-    }
-  };
+  // const onTraySwipeEnd = (event:any) => {
+  //   console.log('End swiping...', event);
+  // };
+
+  // const [offsetTopByDragup, setOffsetTopByDragup] = useState(0);
+  // const [isAssignmentListTrayDragUp, setAssignmentListTrayDragUp] = useState(false);
+  // const [assignmentListTrayDragReset, setAssignmentListTrayDragReset] = useState(false);
+  // const [trayOffsetJustBefore, setTrayOffsetJustBefore] = useState(0);
+  // const assignmentListTray = useRef<HTMLDivElement>(null);
+
+  // const handleSubjectListWrapperOnToucStart = (event: TouchEvent<HTMLDivElement>) => {
+  //   setAssignmentListTrayDragReset(false);
+  //   setTrayOffsetJustBefore(event.changedTouches[0].screenY);
+  // };
+  // const handleSubjectListWrapperOnTouchMove = (event: TouchEvent<HTMLDivElement>) => {
+  //   const currentScreenY = event.changedTouches[0].screenY;
+  //   const isDragginUp = currentScreenY <= trayOffsetJustBefore;
+  //   // const isDragginDown = currentScreenY >= trayOffsetJustBefore;
+
+  //   if (!isAssignmentListTrayDragUp) {
+  //     if (assignmentListTray.current
+  //     && !isAssignmentListTrayDragUp
+  //     && isDragginUp
+  //     && offsetTopByDragup <= 500
+  //     ) {
+  //       const offset = offsetTopByDragup + 1.8;
+  //       setOffsetTopByDragup(offset);
+  //       setTrayOffsetJustBefore(currentScreenY);
+  //     }
+  //     if (assignmentListTray.current
+  //     && !isAssignmentListTrayDragUp
+  //     && !isDragginUp
+  //     && offsetTopByDragup >= 0
+  //     ) {
+  //       const offset = offsetTopByDragup - 1.8;
+  //       setOffsetTopByDragup(offset);
+  //       setTrayOffsetJustBefore(currentScreenY);
+  //     }
+  //   }
+  //   // else if (assignmentListTray.current
+  //   //   && isAssignmentListTrayDragUp
+  //   //   && isDragginDown
+  //   //   && offsetTopByDragup <= 0
+  //   // ) {
+  //   //   console.log(123123);
+  //   //   const offset = offsetTopByDragup - 1.4;
+  //   //   console.log(offset);
+  //   //   setOffsetTopByDragup(offset);
+  //   //   setTrayOffsetJustBefore(currentScreenY);
+  //   // }
+  // };
+
+  // const handleSubjectListWrapperOnTouchEnd = () => {
+  //   if (offsetTopByDragup > 50) {
+  //     setOffsetTopByDragup(0);
+  //     setAssignmentListTrayDragUp(true);
+  //   } else {
+  //     setAssignmentListTrayDragReset(true);
+  //     setOffsetTopByDragup(0);
+  //   }
+  // };
+
+  // const styleToDragUp: CSSProperties = {
+  //   transition: 'all 0.5s ease',
+  //   top: `calc(${topOnDragUp} - ${offsetTopByDragup}px)`,
+  // };
+
+  // const onDragging: CSSProperties = {
+  //   top: `calc(${trayDefaultTop} - ${offsetTopByDragup}px)`,
+  // };
+
+  // const dragReset: CSSProperties = {
+  //   transition: 'all 0.5s ease',
+  //   top: trayDefaultTop,
+  // };
 
   const topOnDragUp = isDeviceHighHighAsIphone8Plus ? '24vh' : '14vh';
 
-  const styleToDragUp: CSSProperties = {
+  const styleOnDragUp: CSSProperties = {
     transition: 'all 0.5s ease',
-    top: `calc(${topOnDragUp} - ${offsetTopByDragup}px)`,
+    top: `calc(${topOnDragUp}`,
+    position: 'absolute',
+    width: '100vw',
   };
 
-  const onDragging: CSSProperties = {
-    top: `calc(${trayDefaultTop} - ${offsetTopByDragup}px)`,
-  };
-
-  const dragReset: CSSProperties = {
-    transition: 'all 0.5s ease',
-    top: trayDefaultTop,
-  };
-
-  const trayOffsetStyle = () => {
-    if (isAssignmentListTrayDragUp) {
-      return styleToDragUp;
-    }
-    if (assignmentListTrayDragReset) {
-      return dragReset;
-    }
-    return onDragging;
-  };
+  const trayStyle = styleOnDragUp;
 
   return (
     <div className={styles.subjectPage}>
@@ -122,15 +134,7 @@ function SubjectPage() {
         <h5 className={styles.professor}>{subject.professor}</h5>
         <p className={styles.introduction}>{subject.introduction}</p>
       </div>
-      <div
-        ref={assignmentListTray}
-        // onTouchStart={handleSubjectListWrapperOnTocuhStart}
-        onTouchStart={(event) => handleSubjectListWrapperOnToucStart(event)}
-        onTouchMove={(event) => handleSubjectListWrapperOnTouchMove(event)}
-        onTouchEnd={handleSubjectListWrapperOnTouchEnd}
-        className={styles.assignmentListWrapper}
-        style={trayOffsetStyle()}
-      >
+      <div style={trayStyle}>
         <AssignmentList />
       </div>
     </div>
