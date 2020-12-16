8a770eb7eb6378c76c64e75fc9c0e5282448c42f
JSKeum <JSKeum2512@gmail.com>
2020-12-16 11:58:24 +0900

    :bug: fix matchMedia issue in safari

--- src/customHooks/UseMediaQuery.tsx
+++ src/customHooks/UseMediaQuery.tsx
@@ -1,13 +1,31 @@
 import { useEffect, useState } from 'react';
+import isUserBrowserSafari from '../utils/DetectBrowser';
 
 const UseMediaQuery = (query: string) => {
   const mediaMatch = matchMedia(query);
   const [matches, setMatches] = useState(mediaMatch.matches);
+
   const mediaHandler = (e: MediaQueryListEvent) => setMatches(e.matches);
 
+  const addBrowserResizeListener = () => {
+    if (isUserBrowserSafari) {
+      mediaMatch.addListener(mediaHandler);
+    } else {
+      mediaMatch.addEventListener('change', mediaHandler);
+    }
+  };
+
+  const removeBrowserResizeListener = () => {
+    if (isUserBrowserSafari) {
+      mediaMatch.removeListener(mediaHandler);
+    } else {
+      mediaMatch.removeEventListener('change', mediaHandler);
+    }
+  };
+
   useEffect(() => {
-    mediaMatch.addEventListener('change', mediaHandler);
-    return () => mediaMatch.removeEventListener('change', mediaHandler);
+    addBrowserResizeListener();
+    return () => removeBrowserResizeListener();
   });
 
   return matches;
---
+++ src/utils/DetectBrowser.ts
@@ -0,0 +1,3 @@
+const isUserBrowserSafari = /Safari/.test(navigator.userAgent) && /Apple Computer/.test(navigator.vendor);
+
+export default isUserBrowserSafari;
