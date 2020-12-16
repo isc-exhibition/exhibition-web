a6fff38e399c772a689bc28ae5d4b317f77183bb
9ummy <godante27@gmail.com>
2020-12-16 22:14:36 +0900

    :art: fix back button

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useHistory, useRouteMatch } from 'react-router-dom';
 import styles from './Header.module.scss';
 import Navbar from '../Navbar/Navbar';
 import LogoButton from '../LogoButton/LogoButton';
 
 function Header() {
+  const history = useHistory();
+  const goBack = () => {
+    history.goBack();
+  };
+
+  const isOnMain = useRouteMatch('/main')?.path === '/main';
+
   return (
     <div className={styles.header}>
-      <Link to="/main" className={styles.backButton}><i className="fas fa-arrow-left" /></Link>
+      {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+      {!isOnMain
+      && (
+      <button className={styles.backButton} type="button" onClick={goBack}>
+        <i className="fas fa-arrow-left" />
+      </button>
+      )}
       <LogoButton />
       <Navbar />
     </div>
