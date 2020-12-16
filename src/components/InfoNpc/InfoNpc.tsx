4f7dc39750538b0a1b38fb512d7482e499159227
vivapyk <vivapyk@gmail.com>
2020-12-16 23:21:29 +0900

    :art: minor css change in faq

--- src/components/InfoNpc/InfoNpc.tsx
+++ src/components/InfoNpc/InfoNpc.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styles from './InfoNpc.module.scss';
 import infoNpc from '../../assets/infonpc.png';
 
 function InfoNpc() {
-  return <img className={styles.infoNpc} src={infoNpc} alt="" />;
+  return <Link to="/faq"><img className={styles.infoNpc} src={infoNpc} alt="" /></Link>;
 }
 
 export default InfoNpc;
