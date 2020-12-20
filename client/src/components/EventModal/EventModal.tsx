17b9c34aed0bb832c4de9eef8a0434121fa7f033
JSKeum <JSKeum2512@gmail.com>
2020-12-20 22:41:46 +0900

    :art: add turtle event

--- client/src/components/EventModal/EventModal.tsx
+++ client/src/components/EventModal/EventModal.tsx
@@ -5,6 +5,7 @@ import Modal from 'react-modal';
 import { EventAnswerResponseData } from '../../pages/EventPage/EventPage';
 import { Letter } from '../EventLetter/letter';
 import styles from './EventModal.module.scss';
+import turtleImage from '../../assets/turtle.png';
 
 interface Props {
     closeModal: () => void;
@@ -21,11 +22,20 @@ function EventModal(props: Props) {
     localStorage.setItem(`ISSHOWNEVENTLETTER${letter?.index}`, 'true');
   };
 
-  const mainTest = letter || eventAnswer?.text;
+  const mainTest = letter?.text || eventAnswer?.text;
 
   const onCloseModal = () => {
     setDismissedLetter();
     closeModal();
+  };
+
+  const turtleEventImage = () => {
+    <div style={{ display: 'flex', alignItems: 'center', margin: '-16px 0 10px' }}>
+      <p>그리고...?!</p>
+      <a href="https://forms.gle/hUa7UPDJr9Fb6sGCA">
+        <img src={turtleImage} alt="거북이" width="110px" height="auto" />
+      </a>
+    </div>;
   };
   return (
     <Modal
@@ -37,6 +47,7 @@ function EventModal(props: Props) {
     >
       {letter && <p>당신이 찾은 글자는...</p> }
       <h1>{mainTest}</h1>
+      {letter?.index === 2 && turtleEventImage}
       {eventAnswer?.isRight && (
       <>
         <p>맞추신 분을 위한 선물</p>
