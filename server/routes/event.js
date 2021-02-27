a3fc2af81ae44b5b704cd937ddbebd231c5f05b6
9ummy <godante27@gmail.com>
2021-02-27 21:35:35 +0900

    :art: remove express & init nest

--- server/routes/event.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const eventControllers = require('../controllers/event');
-
-const router = express.Router();
-
-router.get('/', eventControllers.getEventLetterAnswer);
-
-module.exports = router;
--- server/services/subject.js
+++
@@ -1,9 +0,0 @@
-const Assingment = require('../models/assignment');
-
-const getAssignmentListBySubjectId = async (id) => {
-  const assignmentList = await Assingment.find();
-  const filtered = assignmentList.filter((ass) => ass.subject_id == id);
-  return filtered;
-};
-
-module.exports = getAssignmentListBySubjectId;
