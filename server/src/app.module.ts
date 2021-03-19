0ae4691926cb90d96db15cc97cc28125279587bd
JSKeum <jskeum2512@gmail.com>
2021-03-19 23:04:06 +0900

    [server] :art: add aws secret manager

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,19 +3,22 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AssignmentModule } from './assignment/assignment.module';
 import { Assignment } from './assignment/entity/assignment.entity';
-import { getMongoURI, database } from './config';
-
-const mongoURI = getMongoURI();
+import { getMongoData } from './config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mongodb',
-      url: mongoURI,
-      synchronize: true,
-      useUnifiedTopology: true,
-      database: database,
-      entities: [Assignment],
+    TypeOrmModule.forRootAsync({
+      useFactory: async () => {
+        const { mongoURI, database } = await getMongoData();
+
+        return {
+          type: 'mongodb',
+          url: mongoURI,
+          database: database,
+          synchronize: true,
+          entities: [Assignment],
+        };
+      },
     }),
     AssignmentModule,
     GraphQLModule.forRoot({ autoSchemaFile: true }),
--- server/src/config.ts
+++ server/src/config.ts
@@ -1,19 +1,41 @@
-import * as data from './secret.json';
+import * as AWS from 'aws-sdk';
 
 const { NODE_ENV } = process.env;
 
-export const getMongoURI = () => {
-  const localURI = 'mongodb://localhost';
+const region = 'ap-northeast-2';
+const secretName = 'prod/api/mongo';
 
-  if (NODE_ENV === 'development') {
-    return localURI;
-  } else if (NODE_ENV === 'production') {
-    return data.mongoURI;
-  } else if (NODE_ENV === 'test') {
-    return localURI;
+const localURI = 'mongodb://localhost';
+
+const client = new AWS.SecretsManager({
+  region: region,
+});
+
+export const getMongoData = async () => {
+  let mongoURI = '';
+  let database = '';
+
+  const secret = await client
+    .getSecretValue({ SecretId: secretName })
+    .promise();
+
+  if ('SecretString' in secret) {
+    const secretObject = JSON.parse(secret.SecretString);
+
+    if (NODE_ENV === 'development') {
+      mongoURI = localURI;
+    } else if (NODE_ENV === 'production') {
+      mongoURI = secretObject.mongoURI;
+    } else if (NODE_ENV === 'test') {
+      mongoURI = localURI;
+    } else {
+      mongoURI = localURI;
+    }
+
+    database = secretObject.database;
   } else {
-    return localURI;
+    throw 'SecretString not found';
   }
-};
 
-export const database = data.database;
+  return { mongoURI, database };
+};
