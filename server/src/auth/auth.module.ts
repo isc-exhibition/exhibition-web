4b1f0a3f934f90d56f4dc0388bcc32be545a70cc
JSKeum <jskeum2512@gmail.com>
2021-06-17 17:38:32 +0900

    add JWT auth guard to matation & replace jwt secret data source, AWS -> ENV

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -2,31 +2,32 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AdminRepository } from './admin.repository';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from './jwt.strategy';
-import { getJwtSecretKey} from 'src/config';
-
+import { GQLAuthGuard } from './guard/auth.guard';
+import { User } from './admin.entitiy';
+import '../config';
 
 @Module({
   imports: [
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.registerAsync({
       useFactory: async () => {
-        const JwtSecretKey = await getJwtSecretKey();
+        // const JwtSecretKey = await getJwtSecretKey();
+
         return {
-          secret: JwtSecretKey,       
+          secret: process.env.JWT_SECRET_KEY,
           signOptions: {
             expiresIn: 60 * 60, // 1 hour
           },
-        }
+        };
       },
     }),
-    TypeOrmModule.forFeature([AdminRepository]),
+    TypeOrmModule.forFeature([User]),
   ],
   controllers: [AuthController],
-  providers: [AuthService, JwtStrategy],
+  providers: [AuthService, JwtStrategy, GQLAuthGuard],
   exports: [JwtStrategy, PassportModule],
 })
 export class AuthModule {}
--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,30 +1,72 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { InjectRepository } from '@nestjs/typeorm';
-import { AdminRepository } from './admin.repository';
 import { AdminCredentialsDto } from './dto/auth.admin-credentials.dto';
+import { JwtPayload } from './jwt-payload.interface';
+import * as bcrypt from 'bcrypt';
+import { User } from './admin.entitiy';
+import { MongoRepository } from 'typeorm';
 
 @Injectable()
 export class AuthService {
   constructor(
-    @InjectRepository(AdminRepository)
-    private adminRepository: AdminRepository,
+    @InjectRepository(User)
+    private userRepository: MongoRepository<User>,
     private jwtService: JwtService,
   ) {}
 
-  async signUp(adminCredentialsDto: AdminCredentialsDto): Promise<string> {
-    return this.adminRepository.signUp(adminCredentialsDto);
+  async signIn(adminCredentialsDto: AdminCredentialsDto): Promise<any> {
+    try {
+      const user_id = await this.validateUser(adminCredentialsDto);
+      if (!user_id) {
+        throw new UnauthorizedException('Invalid Id or Password');
+      }
+      const payload: JwtPayload = { user_id };
+      const accessToken = await this.jwtService.sign(payload);
+
+      return { accessToken, user_id };
+    } catch (e) {
+      console.log(e);
+      throw new InternalServerErrorException();
+    }
   }
 
-  async signIn(adminCredentialsDto: AdminCredentialsDto): Promise<any> {
-    const user = await this.adminRepository.validateAdminPassword(
-      adminCredentialsDto,
-    );
-    if (!user) {
-      throw new UnauthorizedException('Invalid Id or Password');
+  async signUp(adminCredentialsDto: AdminCredentialsDto): Promise<string> {
+    const { user_id, password } = adminCredentialsDto;
+
+    const user = new User();
+    user.user_id = user_id;
+    user.salt = await bcrypt.genSalt();
+    user.password = await bcrypt.hash(password, user.salt);
+
+    try {
+      await user.save();
+      return user.user_id;
+    } catch (error) {
+      if (error.code === 11000) {
+        throw new ConflictException('Username already exist');
+      } else {
+        throw new InternalServerErrorException();
+      }
     }
-    const payload = { user };
-    const accessToken = await this.jwtService.sign(payload);
-    return { accessToken, user };
+  }
+
+  private async validateUser(
+    authCredentialsDto: AdminCredentialsDto,
+  ): Promise<string> {
+    const { user_id, password } = authCredentialsDto;
+
+    const user = await this.userRepository.findOne({ user_id });
+
+    if (user && (await user.validatePassword(password))) {
+      return user.user_id;
+    } else {
+      return null;
+    }
   }
 }
